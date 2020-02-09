026c141b23669263207d5ff183579f4a5a727056
tt <traitran44@gmail.com>
2020-02-09 08:38:04 -0800

    Update Cache Read/Write

--- Project1_CPP/src/cache.cpp
+++ Project1_CPP/src/cache.cpp
@@ -25,14 +25,8 @@ void print_cache(vector<cache_set> cache);
 
 void print_cache_dim(vector<cache_set> cache);
 
-void cache_read(cache_set &l1_set, uint64_t l1_tag, cache_set &l2_set, uint64_t l2_tag,
-                uint64_t line_count, sim_stats_t *sim_stats, sim_config_t *sim_conf);
-
-void cache_write(cache_set &l1_set, uint64_t l1_tag, cache_set &l2_set, uint64_t l2_tag,
-                 uint64_t line_count, sim_stats_t *sim_stats, sim_config_t *sim_conf);
-
-void cache_inst(cache_set &l1_set, uint64_t l1_tag, cache_set &l2_set, uint64_t l2_tag,
-                uint64_t line_count, sim_stats_t *sim_stats, sim_config_t *sim_conf);
+void l1_inst_cache_read(cache_set &l1_set, uint64_t l1_tag, cache_set &l2_set, uint64_t l2_tag,
+                        uint64_t line_count, sim_stats_t *sim_stats, sim_config_t *sim_conf);
 
 int cache_miss(cache_set &t_set, uint64_t match_tag);
 
@@ -40,6 +34,20 @@ int cache_read_replace(cache_set &t_set, uint64_t new_tag, uint64_t line_count,
                        sim_config_t *sim_conf, sim_stats_t *sim_stats, cache_level lvl);
 
 int get_victim(cache_set &tag_set, sim_config_t *sim_conf);
+
+int get_first_valid_victim(cache_set &t_set);
+
+void l2_cache_read(cache_set &l2_set, uint64_t l2_tag, uint64_t line_count,
+                   sim_stats_t *sim_stats, sim_config_t *sim_conf);
+
+void l1_data_cache_read(cache_set &l1_set, uint64_t l1_tag, cache_set &l2_set, uint64_t l2_tag,
+                        uint64_t line_count, sim_stats_t *sim_stats, sim_config_t *sim_conf);
+
+void l1_data_cache_write(cache_set &l1_set, uint64_t l1_tag, cache_set &l2_set, uint64_t l2_tag,
+                         uint64_t line_count, sim_stats_t *sim_stats, sim_config_t *sim_conf);
+
+void l2_cache_write(cache_set &l2_set, uint64_t l2_tag, uint64_t line_count,
+                    sim_stats_t *sim_stats, sim_config_t *sim_conf);
 
 // Use this for printing errors while debugging your code
 // Most compilers support the __LINE__ argument with a %d argument type
@@ -147,8 +155,8 @@ void print_cache_dim(vector<cache_set> cache) {
  * @param sim_stats Pointer to simulation statistics structure - Should be populated here
  * @param sim_conf Pointer to the simulation configuration structure - Don't modify it in this function
  */
-void cache_access(uint64_t addr, char type, uint64_t line_count, struct sim_stats_t *sim_stats,
-                  struct sim_config_t *sim_conf) {
+void cache_access(uint64_t addr, char type, uint64_t line_count,
+                  struct sim_stats_t *sim_stats, struct sim_config_t *sim_conf) {
     uint64_t tag_l2 = TAG_MASK(addr, sim_conf->l2unified.c, sim_conf->l2unified.s);
     uint64_t indx_l2 = INDX_MASK(addr, sim_conf->l2unified.b);
     uint64_t tag_d;
@@ -159,17 +167,17 @@ void cache_access(uint64_t addr, char type, uint64_t line_count, struct sim_stat
         case LOAD: // access data cache
             tag_d = TAG_MASK(addr, sim_conf->l1data.c, sim_conf->l1data.s);
             indx_d = INDX_MASK(addr, sim_conf->l1data.b);
-            cache_read(l1_d_cache[indx_d], tag_d, l2_cache[indx_l2], tag_l2, line_count, sim_stats, sim_conf);
+            l1_data_cache_read(l1_d_cache[indx_d], tag_d, l2_cache[indx_l2], tag_l2, line_count, sim_stats, sim_conf);
             break;
         case STORE: // access data cache
             tag_d = TAG_MASK(addr, sim_conf->l1data.c, sim_conf->l1data.s);
             indx_d = INDX_MASK(addr, sim_conf->l1data.b);
-            cache_write(l1_d_cache[indx_d], tag_d, l2_cache[indx_l2], tag_l2, line_count, sim_stats, sim_conf);
+            l1_data_cache_write(l1_d_cache[indx_d], tag_d, l2_cache[indx_l2], tag_l2, line_count, sim_stats, sim_conf);
             break;
         case INST: // access instruction cache
             tag_i = TAG_MASK(addr, sim_conf->l1inst.c, sim_conf->l1inst.s);
             indx_i = INDX_MASK(addr, sim_conf->l1inst.b);
-            cache_inst(l1_i_cache[indx_i], tag_i, l2_cache[indx_l2], tag_l2, line_count, sim_stats, sim_conf);
+            l1_inst_cache_read(l1_i_cache[indx_i], tag_i, l2_cache[indx_l2], tag_l2, line_count, sim_stats, sim_conf);
             break;
         default:
             cout << "Something not right in cache access!" << endl;
@@ -177,8 +185,8 @@ void cache_access(uint64_t addr, char type, uint64_t line_count, struct sim_stat
     }
 }
 
-void cache_inst(cache_set &l1_set, uint64_t l1_tag, cache_set &l2_set, uint64_t l2_tag,
-                uint64_t line_count, sim_stats_t *sim_stats, sim_config_t *sim_conf) {
+void l1_inst_cache_read(cache_set &l1_set, uint64_t l1_tag, cache_set &l2_set, uint64_t l2_tag,
+                        uint64_t line_count, sim_stats_t *sim_stats, sim_config_t *sim_conf) {
     sim_stats->l1inst_num_accesses++;
     int l1_hit_indx;
     int l2_hit_indx;
@@ -203,32 +211,62 @@ void cache_inst(cache_set &l1_set, uint64_t l1_tag, cache_set &l2_set, uint64_t 
     }
 }
 
-void cache_write(cache_set &l1_set, uint64_t l1_tag, cache_set &l2_set, uint64_t l2_tag,
-                 uint64_t line_count, sim_stats_t *sim_stats, sim_config_t *sim_conf) {
-
+/**
+ * WBWA:
+ * L1 Write Hit:
+ *  - set the dirty bit
+ *  - Update tag access count
+ *  - Update tag access time
+ *  - Update replace_q
+ * L1 Write Miss
+ * - Check L2 if hit then copy L2 block into L1 including its metadata
+ * - if L2 miss, then bring in from memory but don't mark it dirty, initialize replacement metadata
+ *
+ * When evict a dirty block:
+ *  - When evicting a dirty block A from L1 we write to L2 (count as L2 cache access)
+        - If the above eviction (Block A eviction) cause L2 to evict another block B then we write the L2 evicted block to memory and update the "num bytes transferred"
+ *
+ * update num bytes transferred when evict a dirty block
+ * WTWNA:
+ * L1 Write Hit:  Update Replacement Metadata, set dirty bit
+ * L1 Write Miss: Do Nothing
+ * L2 Write Hit:  Update Replacement Metadata, set dirty bit
+ * L2 Write Miss: Do Nothing
+ * Always + num bytes transferred since it always write to memory
+ *
+ * @param l1_set
+ * @param l1_tag
+ * @param l2_set
+ * @param l2_tag
+ * @param line_count
+ * @param sim_stats
+ * @param sim_conf
+ */
+void l1_data_cache_write(cache_set &l1_set, uint64_t l1_tag, cache_set &l2_set, uint64_t l2_tag,
+                         uint64_t line_count, sim_stats_t *sim_stats, sim_config_t *sim_conf) {
+    sim_stats->l1data_num_accesses++;
+    sim_stats->l1data_num_accesses_stores++;
+    switch (sim_conf->wp) {
+        case WBWA: // Write back Write Alloc
+            break;
+        case WTWNA: // Write Through Write No Alloc
+            break;
+        default:
+            break;
+    }
 }
 
-void cache_read(cache_set &l1_set, uint64_t l1_tag, cache_set &l2_set, uint64_t l2_tag,
-                uint64_t line_count, sim_stats_t *sim_stats, sim_config_t *sim_conf) {
+
+void l1_data_cache_read(cache_set &l1_set, uint64_t l1_tag, cache_set &l2_set, uint64_t l2_tag,
+                        uint64_t line_count, sim_stats_t *sim_stats, sim_config_t *sim_conf) {
     sim_stats->l1data_num_accesses++;
     sim_stats->l1data_num_accesses_loads++;
     int l1_hit_indx;
-    int l2_hit_indx;
     l1_hit_indx = cache_miss(l1_set, l1_tag);
     if (l1_hit_indx < 0) { // L1 Cache Miss
         sim_stats->l1data_num_misses++;
         sim_stats->l1data_num_misses_loads++;
-        sim_stats->l2unified_num_accesses++;
-        sim_stats->l2unified_num_accesses_loads++;
-        l2_hit_indx = cache_miss(l2_set, l2_tag);
-        if (l2_hit_indx < 0) { // L2 Cache Miss
-            sim_stats->l2unified_num_misses++;
-            sim_stats->l2unified_num_misses_loads++;
-            cache_read_replace(l2_set, l2_tag, line_count, sim_conf, sim_stats, L2);
-        } else { // L2 Cache Hit
-            l2_set.tags[l2_hit_indx].access_count++;
-            l2_set.tags[l2_hit_indx].time = line_count;
-        }
+        l2_cache_read(l2_set, l2_tag, line_count, sim_stats, sim_conf);
         cache_read_replace(l1_set, l1_tag, line_count, sim_conf, sim_stats, L1D);
     } else { // L1 Cache Hit
         l1_set.tags[l1_hit_indx].access_count++;
@@ -236,6 +274,37 @@ void cache_read(cache_set &l1_set, uint64_t l1_tag, cache_set &l2_set, uint64_t 
     }
 }
 
+void l2_cache_read(cache_set &l2_set, uint64_t l2_tag, uint64_t line_count,
+                   sim_stats_t *sim_stats, sim_config_t *sim_conf) {
+    int l2_hit_indx;
+    sim_stats->l2unified_num_accesses++;
+    sim_stats->l2unified_num_accesses_loads++;
+    l2_hit_indx = cache_miss(l2_set, l2_tag);
+    if (l2_hit_indx < 0) { // L2 Cache Miss
+        sim_stats->l2unified_num_misses++;
+        sim_stats->l2unified_num_misses_loads++;
+        cache_read_replace(l2_set, l2_tag, line_count, sim_conf, sim_stats, L2);
+    } else { // L2 Cache Hit
+        l2_set.tags[l2_hit_indx].access_count++;
+        l2_set.tags[l2_hit_indx].time = line_count;
+    }
+}
+
+void l2_cache_write(cache_set &l2_set, uint64_t l2_tag, uint64_t line_count,
+                    sim_stats_t *sim_stats, sim_config_t *sim_conf) {
+
+}
+
+/**
+ * Make sure to cover evicting dirty block case
+ * @param t_set
+ * @param new_tag
+ * @param line_count
+ * @param sim_conf
+ * @param sim_stats
+ * @param lvl
+ * @return
+ */
 int cache_read_replace(cache_set &t_set, uint64_t new_tag, uint64_t line_count,
                        sim_config_t *sim_conf, sim_stats_t *sim_stats, cache_level lvl) {
     int replace_indx = -1;
@@ -266,7 +335,7 @@ int cache_read_replace(cache_set &t_set, uint64_t new_tag, uint64_t line_count,
         t_set.tags[replace_indx].time = line_count;
         t_set.tags[replace_indx].tag_id = new_tag;
     } else {
-        cout << "Replace Indx < 0!" << endl;
+        cout << "Cache Read Replace Indx < 0!" << endl;
     }
     return replace_indx;
 }
@@ -282,7 +351,7 @@ int get_victim(cache_set &tag_set, sim_config_t *sim_conf) {
     uint64_t min_count;
     switch (sim_conf->rp) {
         case LRU:
-            victim = 0;
+            victim = get_first_valid_victim(tag_set);
             min_count = tag_set.tags[victim].time;
             for (uint64_t i = 0; i < tag_set.tag_count; ++i) {
                 if (tag_set.tags[i].time < min_count && tag_set.tags[i].valid) {
@@ -292,7 +361,7 @@ int get_victim(cache_set &tag_set, sim_config_t *sim_conf) {
             }
             break;
         case LFU:
-            victim = 0;
+            victim = get_first_valid_victim(tag_set);
             min_count = tag_set.tags[victim].access_count;
             for (uint64_t i = 0; i < tag_set.tags.size(); ++i) {
                 if (tag_set.tags[i].access_count < min_count) {
@@ -309,8 +378,17 @@ int get_victim(cache_set &tag_set, sim_config_t *sim_conf) {
         default:
             break;
     }
-    if (victim < 0) cout << "get_Victim shouldn't return -1" << endl;
+    if (victim < 0) cout << "get_victim shouldn't return -1" << endl;
     return victim;
+}
+
+int get_first_valid_victim(cache_set &t_set) {
+    for (int i = 0; i < t_set.tags.size(); ++i) {
+        if (t_set.tags[i].valid) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 /**
